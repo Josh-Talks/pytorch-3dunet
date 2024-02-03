c64cf215c44d9ae2d12cd4f842a1377d863ec54d
Joshua Talks <joshua.talks@embl.de>
2024-02-03 16:50:59 +0100

    add wandb logging, formatting

--- pytorch3dunet/train.py
+++ pytorch3dunet/train.py
@@ -1,23 +1,33 @@
 import random
-
+import wandb
 import torch
 
 from pytorch3dunet.unet3d.config import load_config, copy_config
 from pytorch3dunet.unet3d.trainer import create_trainer
 from pytorch3dunet.unet3d.utils import get_logger
 
-logger = get_logger('TrainingSetup')
+logger = get_logger("TrainingSetup")
 
 
 def main():
     # Load and log experiment configuration
     config, config_path = load_config()
+
+    # Setup wandb logger
+    wandb.init(
+        project=config["wandb"]["project"],
+        name=config["wandb"]["name"],
+        config=config,
+        sync_tensorboard=True,
+    )
     logger.info(config)
 
-    manual_seed = config.get('manual_seed', None)
+    manual_seed = config.get("manual_seed", None)
     if manual_seed is not None:
-        logger.info(f'Seed the RNG for all devices with {manual_seed}')
-        logger.warning('Using CuDNN deterministic setting. This may slow down the training!')
+        logger.info(f"Seed the RNG for all devices with {manual_seed}")
+        logger.warning(
+            "Using CuDNN deterministic setting. This may slow down the training!"
+        )
         random.seed(manual_seed)
         torch.manual_seed(manual_seed)
         # see https://pytorch.org/docs/stable/notes/randomness.html
@@ -31,5 +41,5 @@ def main():
     trainer.fit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
