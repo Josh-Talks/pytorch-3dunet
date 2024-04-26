3757416d489575abeceb5dd80ba12b3636549eae
Joshua Talks <joshua.talks@embl.de>
2024-04-26 09:49:24 +0200

    add wandb logging to train and validate

--- pytorch3dunet/train.py
+++ pytorch3dunet/train.py
@@ -17,7 +17,7 @@ def main():
         project=config["wandb"]["project"],
         name=config["wandb"]["name"],
         config=config,
-        sync_tensorboard=True,
+        #sync_tensorboard=True,
     )
     logger.info(config)
 
