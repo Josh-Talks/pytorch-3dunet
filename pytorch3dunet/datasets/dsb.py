d309e09211e5883554a4c06ade7712f7c87835d8
Joshua Talks <joshua.talks@embl.de>
2024-12-28 20:24:30 +0100

    remove asserts

--- pytorch3dunet/datasets/dsb.py
+++ pytorch3dunet/datasets/dsb.py
@@ -892,6 +892,7 @@ class S_BIAD1410_Dataset(ConfigDataset):
         datasets = []
         for i, img_path in enumerate(img_paths):
             try:
+                """
                 if phase == "eval":
                     assert (
                         os.path.basename("_".join(img_path.split("_")[:-1]))
@@ -901,6 +902,7 @@ class S_BIAD1410_Dataset(ConfigDataset):
                     assert (
                         os.path.basename(img_path) in mask_paths[i]
                     ), f"Image {img_path} does not have a corresponding mask in {mask_paths[i]}"
+                """
                 logger.info(f"Loading {phase} set from: {img_path}...")
                 dataset = cls(
                     img_path=img_path,
